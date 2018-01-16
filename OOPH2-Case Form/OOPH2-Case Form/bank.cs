dff7a65532270e3554ee91cb388c2c4971d47e4f
Nidolai <Nidolai@users.noreply.github.com>
2018-01-16 11:06:22 +0100

    Added Konto & Kunde class. Updated bank class

--- OOPH2-Case Form/OOPH2-Case Form/bank.cs
+++ OOPH2-Case Form/OOPH2-Case Form/bank.cs
@@ -9,5 +9,14 @@ namespace OOPH2_Case_Form
 {
     class Bank
     {
+        static void addKunde(string fornavn, string efternavn, int postnr, string adresse, int tlfnr)
+        {
+
+        }
+
+        static void removeKunde(int accountnumber)
+        {
+
+        }
     }
 }
