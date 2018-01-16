ab00a19239cd485dfcb6f225fb363dd914e3f51e
Dark Winther <DarkWinther7@gmail.com>
2018-01-16 13:56:24 +0100

    PostNr part 2

--- Run SQLscript.bat
+++ Run SQLscript.bat
@@ -1,6 +1,6 @@
 ECHO OFF
 sqlcmd -S localhost -E -i "SQL Opret DB og Tabeller.sql"
 ECHO Database og tabeller færdig.
-bcp OOPH2.dbo.PostNr in ./PostNr.csv -c -S . -T -t , -r \n
+bcp OOPH2.dbo.PostNr in ./PostNr.csv -w -S . -T -t , -r \n
 ECHO PostNr ulfyldt fra csv-fil.
 PAUSE
