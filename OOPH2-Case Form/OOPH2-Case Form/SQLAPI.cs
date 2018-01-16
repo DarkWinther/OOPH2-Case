a03d2e6c005939176e5154c934fb3478a9a9d9f4
Nidolai <Nidolai@users.noreply.github.com>
2018-01-16 12:25:22 +0100

    Changed the Database class to SQLAPI class

---
+++ OOPH2-Case Form/OOPH2-Case Form/SQLAPI.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPH2_Case_Form
+{
+    class SQLAPI
+    {
+        static public void Create()
+        {
+            SqlConnection connection = new SqlConnection();
+            SqlCommand cmd;
+            connection.Open();
+
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+
+        static public void Read()
+        {
+            SqlConnection connection = new SqlConnection();
+            SqlCommand cmd;
+            connection.Open();
+
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+
+        static public void Update()
+        {
+            SqlConnection connection = new SqlConnection();
+            SqlCommand cmd;
+            connection.Open();
+
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+
+        static public void Delete()
+        {
+            SqlConnection connection = new SqlConnection();
+            SqlCommand cmd;
+            connection.Open();
+
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+    }
+}
