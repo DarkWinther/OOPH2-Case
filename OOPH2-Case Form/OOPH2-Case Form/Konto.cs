278bc5a77aa6c19f22d962b8358adc5711aa3102
Dark Winther <DarkWinther7@gmail.com>
2018-01-17 09:16:19 +0100

    Konto-constructore

--- OOPH2-Case Form/OOPH2-Case Form/Konto.cs
+++ OOPH2-Case Form/OOPH2-Case Form/Konto.cs
@@ -12,14 +12,31 @@ namespace OOPH2_Case_Form
         private string _typeNavn;
         private float _renteSats;
         private int _kundeNr;
-        private decimal _saldo;
+        private double _saldo;
         private DateTime _oprettelsesdato;
 
         public int kontoNr { get { return _kontoNr; } set { _kontoNr = value; } }
         public string typeNavn { get { return _typeNavn; } set { _typeNavn = value; } }
         public float renteSats { get { return _renteSats; } set { _renteSats = value; } }
-        public int kuneNr { get { return _kundeNr; } set { _kundeNr = value; } }
-        public decimal saldo { get { return _saldo; } set { _saldo = value; } }
+        public int kundeNr { get { return _kundeNr; } set { _kundeNr = value; } }
+        public double saldo { get { return _saldo; } set { _saldo = value; } }
         public DateTime oprettelsesdato { get { return _oprettelsesdato; } set { _oprettelsesdato = value; } }
+
+        public Konto()
+        {
+            oprettelsesdato = DateTime.Now;
+        }
+
+        public Konto(int kontoNr) : this()
+        {
+            this.kontoNr = kontoNr;
+        }
+
+        public Konto(int kontoNr, string typeNavn, float renteSats, double saldo) : this(kontoNr)
+        {
+            this.typeNavn = typeNavn;
+            this.renteSats = renteSats;
+            this.saldo = saldo;
+        }
     }
 }
