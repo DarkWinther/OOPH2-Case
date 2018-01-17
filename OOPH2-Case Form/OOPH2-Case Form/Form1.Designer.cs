9df798ff737c6da8e114f98ecccfd6d901564404
Dark Winther <DarkWinther7@gmail.com>
2018-01-17 12:47:12 +0100

    Form1 knapper

--- OOPH2-Case Form/OOPH2-Case Form/Form1.Designer.cs
+++ OOPH2-Case Form/OOPH2-Case Form/Form1.Designer.cs
@@ -38,20 +38,29 @@
             this.button13 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button5
@@ -59,25 +68,27 @@
             this.button5.Location = new System.Drawing.Point(12, 13);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(102, 23);
-            this.button5.TabIndex = 4;
+            this.button5.TabIndex = 0;
             this.button5.Text = "Opret Kunde";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
             this.button6.Location = new System.Drawing.Point(12, 43);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(102, 23);
-            this.button6.TabIndex = 5;
+            this.button6.TabIndex = 1;
             this.button6.Text = "Fjern kunde";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button8
             // 
             this.button8.Location = new System.Drawing.Point(12, 313);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(75, 23);
-            this.button8.TabIndex = 7;
+            this.button8.TabIndex = 9;
             this.button8.Text = "Vis konto";
             this.button8.UseVisualStyleBackColor = true;
             // 
@@ -86,34 +97,37 @@
             this.button9.Location = new System.Drawing.Point(120, 12);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(102, 23);
-            this.button9.TabIndex = 8;
+            this.button9.TabIndex = 2;
             this.button9.Text = "Opret konto";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
             this.button10.Location = new System.Drawing.Point(120, 42);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(102, 23);
-            this.button10.TabIndex = 9;
+            this.button10.TabIndex = 3;
             this.button10.Text = "Fjern konto";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
             this.button11.Location = new System.Drawing.Point(228, 12);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(102, 23);
-            this.button11.TabIndex = 10;
+            this.button11.TabIndex = 4;
             this.button11.Text = "Hæv beløb";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
             this.button12.Location = new System.Drawing.Point(228, 42);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(102, 23);
-            this.button12.TabIndex = 11;
+            this.button12.TabIndex = 5;
             this.button12.Text = "Indsæt beløb";
             this.button12.UseVisualStyleBackColor = true;
             // 
@@ -122,22 +136,23 @@
             this.button13.Location = new System.Drawing.Point(93, 313);
             this.button13.Name = "button13";
             this.button13.Size = new System.Drawing.Size(153, 23);
-            this.button13.TabIndex = 12;
+            this.button13.TabIndex = 10;
             this.button13.Text = "Udskriv transaktioner";
             this.button13.UseVisualStyleBackColor = true;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(375, 11);
+            this.comboBox1.Location = new System.Drawing.Point(356, 11);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(195, 24);
-            this.comboBox1.TabIndex = 13;
+            this.comboBox1.Size = new System.Drawing.Size(214, 24);
+            this.comboBox1.TabIndex = 6;
             this.comboBox1.Text = "Vælg kunde:";
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.comboBox2);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
@@ -152,42 +167,107 @@
             this.panel1.Location = new System.Drawing.Point(12, 72);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(318, 195);
-            this.panel1.TabIndex = 14;
+            this.panel1.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Telefon nummer";
+            this.label5.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Post nummer";
+            this.label4.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Adresse";
+            this.label3.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Efternavn";
+            this.label2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fornavn";
+            this.label1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(129, 106);
+            this.textBox5.Location = new System.Drawing.Point(129, 134);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(178, 22);
-            this.textBox5.TabIndex = 4;
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Visible = false;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(129, 78);
+            this.textBox4.Location = new System.Drawing.Point(129, 106);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(178, 22);
-            this.textBox4.TabIndex = 3;
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Visible = false;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(129, 134);
+            this.textBox3.Location = new System.Drawing.Point(129, 78);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(178, 22);
-            this.textBox3.TabIndex = 2;
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Visible = false;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(129, 50);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(178, 22);
-            this.textBox2.TabIndex = 1;
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Visible = false;
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(129, 21);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(178, 22);
-            this.textBox1.TabIndex = 0;
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Visible = false;
             // 
             // dataGridView1
             // 
@@ -196,67 +276,92 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(558, 199);
-            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.TabIndex = 11;
             // 
-            // button1
+            // groupBox1
             // 
-            this.button1.Location = new System.Drawing.Point(232, 162);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Submit";
-            this.button1.UseVisualStyleBackColor = true;
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Location = new System.Drawing.Point(356, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(214, 185);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kundeinformation";
             // 
-            // label1
+            // label7
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 17);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Fornavn";
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 24);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "KundeNr: ";
             // 
-            // label2
+            // label6
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 53);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 17);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Efternavn";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(353, 316);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Samlet Saldo:";
             // 
-            // label3
+            // label8
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 81);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(91, 17);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Post nummer";
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 53);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 17);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Navn: ";
             // 
-            // label4
+            // label9
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 109);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 17);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Adresse";
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 81);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(68, 17);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Adresse: ";
             // 
-            // label5
+            // label10
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(3, 137);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(111, 17);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Telefon nummer";
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 109);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(59, 17);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "PostNr: ";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(3, 162);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(178, 24);
+            this.comboBox2.TabIndex = 14;
+            this.comboBox2.Visible = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(6, 137);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 17);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "TlfNr: ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(582, 553);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.comboBox1);
@@ -275,7 +380,10 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -302,6 +410,14 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label11;
     }
 }
 
--- OOPH2-Case Form/OOPH2-Case Form/Form1.cs
+++ OOPH2-Case Form/OOPH2-Case Form/Form1.cs
@@ -13,6 +13,9 @@ namespace OOPH2_Case_Form
 {
     public partial class Form1 : Form
     {
+        private SqlDataAdapter adapter = new SqlDataAdapter("SELECT Fornavn FROM Kunde;", SQLAPI.connection);
+        private DataTable table = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
@@ -20,11 +23,73 @@ namespace OOPH2_Case_Form
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            SqlDataAdapter adapter = new SqlDataAdapter("SELECT Fornavn FROM Kunde;", SQLAPI.connection);
-            DataTable table = new DataTable();
             adapter.Fill(table);
             comboBox1.DataSource = table;
             comboBox1.DisplayMember = "Fornavn";
+        }
+
+        //Opret Kunde
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Show(label1, label2, label3, label4, label5, textBox1, textBox2, textBox3, textBox4, textBox5, button1);
+            Hide(comboBox2);
+            label1.Text = "Fornavn";
+            label2.Text = "Efternavn";
+            label3.Text = "Adresse";
+            label4.Text = "Post nummer";
+            label5.Text = "Telefon nummer";
+            button1.Text = "Tilføj kunde";
+        }
+
+        //Fjern Kunde
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Show(label1, textBox1, button1);
+            Hide(label2, label3, label4, label5, textBox2, textBox3, textBox4, textBox5, comboBox2);
+            label1.Text = "KundeNr";
+            button1.Text = "Fjern Kunde";
+        }
+
+        //Opret konto
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Show(label1, label2, textBox1, comboBox2, button1);
+            Hide(label3, label4, label5, textBox2, textBox3, textBox4, textBox5);
+            label1.Text = "KundeNr";
+            label2.Text = "KontoType";
+            button1.Text = "Tilføj Konto";
+            comboBox2.Location = textBox2.Location;
+        }
+
+        //Fjern Konto
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Hide(label2, label3, label4, label5, textBox2, textBox3, textBox4, textBox5, comboBox2);
+            Show(label1, textBox1, button1);
+            button1.Text = "Fjern Konto";
+            label1.Text = "KontoNr";
+        }
+
+        //Hæv beløb
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Hide(params Control[] things)
+        {
+            foreach (var item in things)
+            {
+                item.Visible = false;
+            }
+        }
+
+        private void Show(params Control[] things)
+        {
+            foreach (var item in things)
+            {
+                item.Visible = true;
+            }
         }
     }
 }
