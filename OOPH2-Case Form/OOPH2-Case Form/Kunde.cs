c2a910b697c3be28efcc583335b3ab8160fe9f35
Dark Winther <DarkWinther7@gmail.com>
2018-01-17 08:24:42 +0100

    Opret Kunde-klasse

---
+++ OOPH2-Case Form/OOPH2-Case Form/Kunde.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPH2_Case_Form
+{
+    class Kunde
+    {
+        private int _kundeNr;
+        private string _fornavn;
+        private string _efternavn;
+        private int _postNr;
+        private string _adresse;
+        private int _tlfNr;
+        private DateTime _oprettelsesdato;
+
+        public int kundeNr { get { return _kundeNr; } set { _kundeNr = value; } }
+        public string fornavn { get { return _fornavn; } set { _fornavn = value; } }
+        public string efternavn { get { return _efternavn; } set { _efternavn = value; } }
+        public int postNr { get { return _postNr; } set { _postNr = value; } }
+        public string adresse { get { return _adresse; } set { _adresse = value; } }
+        public int tlfNr { get { return _tlfNr; } set { _tlfNr = value; } }
+        public DateTime oprettelsesdato { get { return _oprettelsesdato; } set { _oprettelsesdato = value; } }
+    }
+}
--- Run SQLscript.bat
+++ Run SQLscript.bat
@@ -2,5 +2,5 @@ ECHO OFF
 sqlcmd -S localhost -E -i "SQL Opret DB og Tabeller.sql"
 ECHO Database og tabeller færdig.
 bcp OOPH2.dbo.PostNr in ./PostNr.csv -w -S . -T -t , -r \n
-ECHO PostNr ulfyldt fra csv-fil.
+ECHO PostNr udfyldt fra csv-fil.
 PAUSE
