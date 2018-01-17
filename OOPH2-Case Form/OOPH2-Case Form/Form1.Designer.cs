e5908d63d1cbe5377a6010f476db5048652599c8
Dark Winther <DarkWinther7@gmail.com>
2018-01-17 09:34:38 +0100

    Form1 udslag (knapper)

--- OOPH2-Case Form/OOPH2-Case Form/Form1.Designer.cs
+++ OOPH2-Case Form/OOPH2-Case Form/Form1.Designer.cs
@@ -32,12 +32,21 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(79, 22);
+            this.button1.Location = new System.Drawing.Point(432, 355);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(126, 40);
             this.button1.TabIndex = 0;
@@ -48,7 +57,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(79, 68);
+            this.button2.Location = new System.Drawing.Point(432, 401);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(126, 40);
             this.button2.TabIndex = 1;
@@ -59,9 +68,9 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(79, 114);
+            this.button3.Location = new System.Drawing.Point(410, 447);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(126, 40);
+            this.button3.Size = new System.Drawing.Size(148, 40);
             this.button3.TabIndex = 2;
             this.button3.Text = "Update Kevin";
             this.button3.UseVisualStyleBackColor = true;
@@ -70,7 +79,7 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(79, 160);
+            this.button4.Location = new System.Drawing.Point(432, 493);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(126, 40);
             this.button4.TabIndex = 3;
@@ -78,11 +87,101 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(40, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(102, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Opret Kunde";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(40, 42);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(102, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Fjern kunde";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(344, 42);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(139, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Vis Kundeoversigt";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(229, 25);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Vis konto";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(40, 90);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(102, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Opret konto";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(40, 120);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(102, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Fjern konto";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(40, 171);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(102, 23);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "Hæv beløb";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(40, 201);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(102, 23);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "Indsæt beløb";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(267, 170);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(153, 23);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "Udskriv transaktioner";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(582, 553);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -100,6 +199,15 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
     }
 }
 
