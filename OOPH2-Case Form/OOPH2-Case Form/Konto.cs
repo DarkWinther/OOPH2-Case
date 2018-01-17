07554ce2c14bdabadc207310979f3795d2f67d05
Dark Winther <DarkWinther7@gmail.com>
2018-01-17 08:30:21 +0100

    Opret konto-klasse

---
+++ OOPH2-Case Form/OOPH2-Case Form/Konto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPH2_Case_Form
+{
+    class Konto
+    {
+        private int _kontoNr;
+        private short _kontoType;
+        private int _kundeNr;
+        private decimal _saldo;
+        private DateTime _oprettelsesdato;
+
+        public int kontoNr { get { return _kontoNr; } set { _kontoNr = value; } }
+        public short kontoType { get { return _kontoType; } set { _kontoType = value; } }
+        public int kuneNr { get { return _kundeNr; } set { _kundeNr = value; } }
+        public decimal saldo { get { return _saldo; } set { _saldo = value; } }
+        public DateTime oprettelsesdato { get { return _oprettelsesdato; } set { _oprettelsesdato = value; } }
+    }
+}
